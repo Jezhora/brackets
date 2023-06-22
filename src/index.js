536f0f9fb5b4312684c5783c2a14d295565139e3
Jezhora <jezhora.photo@gmail.com>
2023-06-22 18:44:50 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function check(str, bracketsConfig) {
+
+const string = '([{}])';
+const brackets = [['(', ')'], ['[', ']'], ['{', '}']]
+
+// module.exports = function check(str, bracketsConfig) {
+  function check(str, bracketsConfig) {
   // your solution
+  let bracketsPair = {};
+  let bracketsOpen = []
+    
+  bracketsConfig.forEach(element => {
+    bracketsPair[element[1]] = element[0]
+    bracketsOpen.push(element[0])
+  })
+
+  return bracketsOpen
+ 
 }
+
+console.log(check(string, brackets))
