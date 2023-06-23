86d3c3ba451fa527c30bcaf6d5f2b6cdc494046e
Jezhora <jezhora.photo@gmail.com>
2023-06-23 22:10:27 +0300

    feat: all tests passed

--- src/index.js
+++ src/index.js
@@ -1,44 +1,32 @@
-
-// const string = '|()|(||)||';
-// const brackets = [['(', ')'], ['|', '|']];
-
 module.exports = function check(str, bracketsConfig) {
-  // function check(str, bracketsConfig) {
-  // your solution
+
   let bracketsPair = {};
   let bracketsOpen = [];
-
+  let bracketsClose = [];
+  
     
   bracketsConfig.forEach(element => {
     bracketsPair[element[1]] = element[0];
     bracketsOpen.push(element[0]);
+    bracketsClose.push(element[1]);
+    
   })
   
-  
-  let stackSymbol = [];
+  let stackSymbol = []; debugger
 
   for (let i = 0; i < str.length; i++){
     let currentSymbol = str[i];
+    let topElement = stackSymbol[stackSymbol.length -1]
 
-    if(bracketsOpen.includes(currentSymbol)) {
-      stackSymbol.push(currentSymbol);
+    if(stackSymbol.length > 0 && bracketsClose.includes(currentSymbol) 
+        && bracketsClose.indexOf(currentSymbol) === bracketsOpen.indexOf(topElement)) {
+        stackSymbol.pop();debugger
+    } else if(bracketsOpen.includes(currentSymbol)) {
+      stackSymbol.push(currentSymbol);debugger
     } else {
-      if(stackSymbol.length === 0) {
-        return false;
-      }
-
-      let topElement = stackSymbol[stackSymbol.length -1]
-
-      if (bracketsPair[currentSymbol] === topElement) {
-        stackSymbol.pop(); 
-      }  else {
-        return false;
-      }
+      return false; 
     }
   }
  
   return stackSymbol.length === 0;
- 
 }
-
-// console.log(check(string, brackets))
