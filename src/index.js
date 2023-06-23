1854597d338ccee122a2bbf9146d303842add91f
Jezhora <jezhora.photo@gmail.com>
2023-06-23 10:20:30 +0300

    does not passed test if key == value

--- src/index.js
+++ src/index.js
@@ -1,20 +1,44 @@
 
-const string = '([{}])';
-const brackets = [['(', ')'], ['[', ']'], ['{', '}']]
+// const string = '|()|(||)||';
+// const brackets = [['(', ')'], ['|', '|']];
 
-// module.exports = function check(str, bracketsConfig) {
-  function check(str, bracketsConfig) {
+module.exports = function check(str, bracketsConfig) {
+  // function check(str, bracketsConfig) {
   // your solution
   let bracketsPair = {};
-  let bracketsOpen = []
+  let bracketsOpen = [];
+
     
   bracketsConfig.forEach(element => {
-    bracketsPair[element[1]] = element[0]
-    bracketsOpen.push(element[0])
+    bracketsPair[element[1]] = element[0];
+    bracketsOpen.push(element[0]);
   })
+  
+  
+  let stackSymbol = [];
 
-  return bracketsOpen
+  for (let i = 0; i < str.length; i++){
+    let currentSymbol = str[i];
+
+    if(bracketsOpen.includes(currentSymbol)) {
+      stackSymbol.push(currentSymbol);
+    } else {
+      if(stackSymbol.length === 0) {
+        return false;
+      }
+
+      let topElement = stackSymbol[stackSymbol.length -1]
+
+      if (bracketsPair[currentSymbol] === topElement) {
+        stackSymbol.pop(); 
+      }  else {
+        return false;
+      }
+    }
+  }
+ 
+  return stackSymbol.length === 0;
  
 }
 
-console.log(check(string, brackets))
+// console.log(check(string, brackets))
